d40f08057d14a8c9ab9c983cbbe9cbfefefb1c7f
dicdicjalique <dicdic_14@yahoo.com>
2014-10-21 22:15:58 +0800

    Revert 6d6a6b5..ff44f00
    This rolls back to commit 6d6a6b5d9c4b0368ca7270bc472498382e4699b9.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -41,6 +41,11 @@ class UserController < ApplicationController
 	  end
 
 	  if user_info != nil && user_info.id != nil
+
+p user_info.id
+p user_info.email
+p user_info.name
+
 	    begin
 	    user = User.find(user_info.id)	# if user is authorized to use app
 	    session[:user_id] = user_info.id
@@ -78,7 +83,6 @@ class UserController < ApplicationController
 
 	def destroy # add user from requests - destroy from requests
 		@request = Request.find(params[:id])
-p @request
 		user = User.new
 		user.user_id = @request.user_id
 		user.email = @request.email
