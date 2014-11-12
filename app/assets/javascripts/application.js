205e41d05bb50296ea561dd249c78c7beaf3e6ad
dicdicjalique <dicdicjalique@Dicdics-Mac-Pro.local>
2014-11-13 06:35:38 +0800

    added sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap.min
+//= require plugins/metisMenu/metisMenu.min
+//= require custom_js
 //= require turbolinks
 //= require_tree .
