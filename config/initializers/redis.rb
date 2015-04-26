f107610f9510bc783403a56450eabac5cc4c1296
christianeyee <christiane.yee@gmail.com>
2015-04-26 20:38:37 +0800

    Configured Redis for Heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
-if ENV["REDISCLOUD_URL"]
-  $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+if ENV["REDISTOGO_URL"]
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  $redis = Redis.new(:url => uri)
 else
   $redis = Redis.new(:host => 'localhost', :port => '6379')
 end
