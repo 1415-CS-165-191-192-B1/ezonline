664e8812e04ddf98b800f71c54538207b1614f72
christianeyee <christiane.yee@gmail.com>
2015-05-14 16:48:38 +0800

    Added non-working test

---
+++ spec/views/file/edit_snippet_spec.rb
@@ -0,0 +1,10 @@
+feature 'User edits a snippet' do
+  scenario 'they see the foobar on the page' do
+    visit 'http://ezonline.herokuapp.com/file/21/edit'
+
+    fill_in 'text_commit_text', with: 'My foobar'
+    click_button 'Save'
+
+    expect(page).to have_text 'Update saved'
+  end
+end
